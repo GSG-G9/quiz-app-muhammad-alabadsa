eeb8e3f035b4ce5e3862a96c9366f1145e26e8e7
mabadsa <moh22389@gmail.com>
2020-10-21 15:59:42 +0300

    Complete the question Stage

--- assets/main.js
+++ assets/main.js
@@ -4,81 +4,147 @@ import startSection from "./page-layout/start.js";
 import contentSection from "./page-layout/content.js";
 import selectLanguage from "./quiz-options/language.js";
 import questionsSection from "./quiz-questions/quiz-questions.js";
+import questions from "./quiz-questions/questions.js";
+
+const $rootContainer = document.getElementById("root");
+let $resultsContainer;
+let $submitButton;
+let $quizQuestionsContainer;
+let $optionsArea;
+let $progressBar;
+let $quizInfo;
+let $backToLanguageList;
+let $resulContainer;
+let $languageSelectContainer;
+let $quizQuestions;
 
 const state = {
   selectZone: false,
   questions: [],
 };
 
-let currentIndex = 0;
-
-const questions = [
-  {
-    lang: "javascript",
-    questions: [
-      {
-        question: "AAAAA",
-        opt_1: "Option_1",
-        opt_2: "Option_2",
-        opt_3: "Option_3",
-        opt_4: "Option_4",
-        correctAnswer: "Option_3",
-      },
-      {
-        question: "BBBBB",
-        opt_1: "Option_1",
-        opt_2: "Option_2",
-        opt_3: "Option_3",
-        opt_4: "Option_4",
-        correctAnswer: "Option_3",
-      },
-      {
-        question: "CCCCCC",
-        opt_1: "Option_1",
-        opt_2: "Option_2",
-        opt_3: "Option_3",
-        opt_4: "Option_4",
-        correctAnswer: "Option_3",
-      },
-    ],
-  },
-  {
-    lang: "css",
-    questions: {
-      question: "AAAAA",
-      options: ["op_1", "op_2", "pp_3"],
-      correctAnswer: "op_2",
-    },
-  },
-  {
-    lang: "html",
-    questions: {
-      question: "AAAAA",
-      options: ["op_1", "op_2", "pp_3"],
-      correctAnswer: "op_2",
-    },
-  },
-  {
-    lang: "nodejs",
-    questions: {
-      question: "AAAAA",
-      options: ["op_1", "op_2", "pp_3"],
-      correctAnswer: "op_2",
-    },
-  },
-  {
-    lang: "reactjs",
-    questions: {
-      question: "AAAAA",
-      options: ["op_1", "op_2", "pp_3"],
-      correctAnswer: "op_2",
-    },
-  },
-];
-
 localStorage.setItem("quizapp", JSON.stringify(questions));
 
-const $rootContainer = document.getElementById("root");
+let currentIndex = 0;
+let rightAnswersCount = 0;
+
+function showResult(questionCount) {
+  let theResults;
+
+  if (currentIndex === questionCount) {
+    $quizQuestionsContainer.remove();
+    $optionsArea.remove();
+    $submitButton.remove();
+    $progressBar.remove();
+    $quizInfo.remove();
+
+    if (
+      rightAnswersCount > questionCount / 2 &&
+      rightAnswersCount < questionCount
+    ) {
+      theResults = `
+        <div class="result-container">
+          <div class="message-container">
+            <span class="message good">Good</span>
+            <span class="message--desc good">Good But You Have To Work Hard!</span>
+          </div>
+          <div class="correct-answer">
+            <div>Correct Answer</div>
+            <div>
+              <span>${rightAnswersCount}</span>
+            </div>
+          </div>
+          <div class="wrong-answer">
+            <div>Wrong Answer</div>
+            <div>
+              <span>${questionCount - rightAnswersCount}</span>
+            </div>
+          </div>
+          <div class="back-language-list">
+            <button class="back-language-list__btn">Language List</button>
+          </div>
+        </div>
+      `;
+    } else if (rightAnswersCount === questionCount) {
+      theResults = `
+      <div class="result-container">
+        <div class="message-container">
+          <span class="message perfect">Perfect</span>
+          <span class="message--desc">All Answer Is Correct.</span>
+        </div>
+        <div class="correct-answer">
+          <div>Correct Answer</div>
+          <div>
+            <span>${rightAnswersCount}</span>
+          </div>
+        </div>
+        <div class="wrong-answer">
+          <div>Correct Answer</div>
+          <div>
+            <span>${questionCount - rightAnswersCount}</span>
+          </div>
+        </div>
+        <div class="back-language-list">
+          <button class="back-language-list__btn">Language List</button>
+        </div>
+      </div>
+    `;
+    } else {
+      theResults = `
+      <div class="result-container">
+        <div class="message-container">
+          <span class="message bad">Bad</span>
+          <span class="message--desc bad">Most of the answers are wrong.</span>
+        </div>
+        <div class="correct-answer">
+          <div>Correct Answer</div>
+          <div>
+            <span>${rightAnswersCount}</span>
+          </div>
+        </div>
+        <div class="wrong-answer">
+          <div>Wrong Answer</div>
+          <div>
+            <span>${questionCount - rightAnswersCount}</span>
+          </div>
+        </div>
+        <div class="back-language-list">
+          <button class="back-language-list__btn">Language List</button>
+        </div>
+      </div>
+    `;
+    }
+    // const textNode = document.createTextNode('')
+    $resultsContainer.innerHTML = theResults;
+    $backToLanguageList = document.querySelector(".back-language-list");
+    $languageSelectContainer.classList.remove("fade-out");
+    $backToLanguageList.addEventListener("click", function () {
+      $resulContainer.classList.add("fade-out");
+      setTimeout(function () {
+        // $resulContainer.innerHTML = "";
+        $quizQuestions.style.display = "none";
+        $languageSelectContainer.classList.remove("block-none");
+      }, 500);
+    });
+    // $resultsContainer.style.padding = "10px";
+    // $resultsContainer.style.backgroundColor = "white";
+    // $resultsContainer.style.marginTop = "10px";
+  }
+}
+
+function checkAnswer(rightAnswer, questionsCount) {
+  let answer = document.getElementsByName("question");
+  let theChoosenAnswer;
+
+  for (let i = 0; i < answer.length; i++) {
+    if (answer[i].checked) {
+      theChoosenAnswer = answer[i].dataset.answer;
+    }
+  }
+  if (rightAnswer === theChoosenAnswer) {
+    rightAnswersCount++;
+  }
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
   $rootContainer.innerHTML = layoutPage;
@@ -91,8 +157,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
   $contentQuiz.innerHTML += selectLanguage;
   $contentQuiz.innerHTML += questionsSection;
 
-  const $languageSelectContainer = document.getElementById("language");
-  const $quizQuestions = document.getElementById("quiz-questions");
+  $languageSelectContainer = document.getElementById("language");
+  $quizQuestions = document.getElementById("quiz-questions");
 
   $languageSelectContainer.classList.add("block-none");
   $quizQuestions.classList.add("block-none");
@@ -136,27 +202,49 @@ document.addEventListener("DOMContentLoaded", function (e) {
       console.log(questions);
       const $categoryLang = document.querySelector(".category span");
       const $questionsCountElm = document.querySelector(".count span");
-      const $quizQuestionsContainer = document.querySelector(
+      $quizQuestionsContainer = document.querySelector(
         ".quiz-questions-container .question-area"
       );
-      const $submitButton = document.querySelector(".submit-button");
-      const $optionsArea = document.querySelector(".options-area");
+      $submitButton = document.querySelector(".submit-button");
+      $optionsArea = document.querySelector(".options-area");
+      $resultsContainer = document.querySelector(".results");
+      $progressBar = document.querySelector(".progress-bar");
+      $quizInfo = document.querySelector(".quiz-info");
+      // $backToLanguageList = document.querySelector(".back-language-list");
+      // $resulContainer = document.querySelector(".result-container");
+      let $progressBarJuice = document.querySelector(".progress-bar .span");
+
+      state.questions[0].questions.sort(() => 0.5 - Math.random()).length = 10;
 
       $submitButton.addEventListener("click", function (e) {
+        let correctAnswer = questions[0].questions[currentIndex].correctAnswer;
+
         currentIndex++;
+        console.log(currentIndex);
+        // console.log(correctAnswer);
+
+        checkAnswer(correctAnswer, questions[0].questions.length);
 
         $quizQuestionsContainer.innerHTML = "";
         $optionsArea.innerHTML = "";
+
+        $progressBarJuice.style.width = currentIndex + "0%";
 
         renderQuestions(
           questions[0].questions[currentIndex],
           questions[0].lang,
           questions[0].questions.length
         );
+
+        showResult(questions[0].questions.length);
+        $resulContainer = document.querySelector(".result-container");
       });
 
       const renderQuestions = function (questions, lang, questionsCount) {
         if (currentIndex < questionsCount) {
+          console.log(currentIndex);
+          console.log(questionsCount);
+
           $categoryLang.innerHTML =
             lang.slice(0, 1).toUpperCase() + lang.slice(1);
           $questionsCountElm.innerHTML = questionsCount;
@@ -197,47 +285,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
           }
         }
       };
-      /*
-        const questionsCounc = questions[0].length;
-      const $answerArea = document.querySelector(".options-area");
-
-      let $questionsTitle = document.createElement("h3");
-
-      let questionText = document.createTextNode(
-        questions[0].questions.question
-      );
-
-      console.log(questions[0].questions.question);
-
-      $questionsTitle.appendChild(questionText);
-      $quizQuestionsContainer.appendChild($questionsTitle);
-
-      for (let i = 1; i <= 4; i++) {
-        let $answersContainer = document.createElement("div");
-
-        $answersContainer.className = "option";
-
-        let $radioButton = document.createElement("input");
-
-        // Add type, name and id to radio button
-        $radioButton.name = "question";
-        $radioButton.type = "radio";
-        $radioButton.id = `opt_${i}`;
-        $radioButton.dataset.answer = questions[0].questions[`opt_${i}`];
-
-        i === 1 ? ($radioButton.checked = true) : false;
-
-        let $theLabel = document.createElement("label");
-        $theLabel.htmlFor = `opt_${i}`;
-        let labelText = document.createTextNode(questions[0].questions[`opt_${i}`]);
-        $theLabel.appendChild(labelText);
-        $answersContainer.appendChild($radioButton);
-        $answersContainer.appendChild($theLabel);
-
-        // Add The Answer Container Div to Answer Area Div
-        $answerArea.appendChild($answersContainer);
-      }
-      */
       renderQuestions(
         questions[0].questions[currentIndex],
         questions[0].lang,
@@ -251,6 +298,13 @@ document.addEventListener("DOMContentLoaded", function (e) {
         $languageSelectContainer.classList.add("block-none");
         $quizQuestions.classList.remove("block-none");
       }, 300);
+      // $backToLanguageList.addEventListener("click", function () {
+      //   $resulContainer.classList.add("fade-out");
+
+      //   setTimeout(function () {
+      //     $resulContainer.classList.add("block-none");
+      //   }, 500);
+      // });
     });
   });
 
--- assets/quiz-questions/quiz-questions.js
+++ assets/quiz-questions/quiz-questions.js
@@ -1,5 +1,8 @@
 const questionsSection = `
     <div class="quiz-questions-container" id="quiz-questions">
+        <div class="progress-bar">
+            <div class="span"></div>
+        </div>
         <div class="quiz-info">
         <div class="category">Category: <span>HTML</span></div>
         <div class="count">Questions Count: <span></span></div>
@@ -7,10 +10,6 @@ const questionsSection = `
         <div class="question-area"></div>
         <div class="options-area"></div>
         <button class="submit-button">Submit Answer</button>
-        <div class="bullets">
-            <div class="spans"></div>
-            <div class="countdown"></div>
-        </div>
         <div class="results"></div>
     </div>
 `;
